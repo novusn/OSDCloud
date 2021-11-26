75dffc2f6cdb7f1d41c8ebd8ff90b7b739402a21
Mark Birchall <mark.birchall@novus.co.uk>
2021-11-26 11:22:29 +0000

    Version 1.0

---
+++ DefaultOSDCloud.ps1
@@ -0,0 +1,36 @@
+Write-Host  -ForegroundColor Cyan "Starting OSDCloud ..."
+Start-Sleep -Seconds 5
+
+#Change Display Resolution for Virtual Machine
+if ((Get-MyComputerModel) -match 'Virtual') {
+    Write-Host  -ForegroundColor Cyan "Setting Display Resolution to 1366x"
+    Set-DisRes 1366
+}
+cls
+Write-Host "===================== Main Menu =======================" -ForegroundColor Yellow
+Write-Host "1: Zero-Touch WIN10 20H2 | en-gb | Enterprise"-ForegroundColor Yellow
+Write-Host "2: Zero-Touch WIN10 21H2 | en-gb | Enterprise" -ForegroundColor Yellow
+Write-Host "3: OSDCloudGUI | Testing" -ForegroundColor Yellow
+Write-Host "4: Exit" -ForegroundColor Yellow
+$input = Read-Host "Please make a selection"
+Write-Host  -ForegroundColor Yellow "Loading OSDCloud..."
+
+#Make sure I have the latest OSD Content
+Write-Host  -ForegroundColor Cyan "Updating OSD PowerShell Module"
+Install-Module OSD -Force
+
+Write-Host  -ForegroundColor Cyan "Importing PowerShell Modules"
+Import-Module OSD -Force
+
+switch ($input)
+{
+    '1' { Start-OSDCloud -OSLanguage en-gb -OSBuild 20H2 -OSEdition Enterprise -ZTI } 
+    '2' { Start-OSDCloud -OSLanguage en-gb -OSBuild 21H2 -OSEdition Enterprise -ZTI } 
+    '3' { Start-OSDCloudGUI	} 
+    '4' { Exit }
+}
+
+#Restart from WinPE
+Write-Host  -ForegroundColor Cyan "Restarting in 20 seconds!"
+Start-Sleep -Seconds 20
+wpeutil reboot
