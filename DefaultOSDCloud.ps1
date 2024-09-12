666b606881fa393a405253725cb6adbbda7962f2
Mark Birchall <mark.birchall@novus.co.uk>
2024-09-12 12:25:02 +0100

    Removed Windows 11 22H2 as its EOL

--- DefaultOSDCloud.ps1
+++ DefaultOSDCloud.ps1
@@ -10,12 +10,10 @@ cls
 Write-Host "===================== Main Menu ======================="
 Write-Host "1: Zero-Touch WIN10 22H2 | en-gb | Education" -ForegroundColor Yellow
 Write-Host "2: Zero-Touch WIN10 22H2 | en-gb | Enterprise" -ForegroundColor Green
-Write-Host "3: Zero-Touch WIN11 22H2 | en-gb | Education" -ForegroundColor Yellow
-Write-Host "4: Zero-Touch WIN11 22H2 | en-gb | Enterprise" -ForegroundColor Green
-Write-Host "5: Zero-Touch WIN11 23H2 | en-gb | Education" -ForegroundColor Yellow
-Write-Host "6: Zero-Touch WIN11 23H2 | en-gb | Enterprise" -ForegroundColor Green
-Write-Host "7: Zero-Touch WIN11 23H2 | en-gb | Professional" -ForegroundColor Magenta
-Write-Host "8: OSDCloudGUI" -ForegroundColor Cyan
+Write-Host "3: Zero-Touch WIN11 23H2 | en-gb | Education" -ForegroundColor Yellow
+Write-Host "4: Zero-Touch WIN11 23H2 | en-gb | Enterprise" -ForegroundColor Green
+Write-Host "5: Zero-Touch WIN11 23H2 | en-gb | Professional" -ForegroundColor Magenta
+Write-Host "6: OSDCloudGUI" -ForegroundColor Cyan
 Write-Host "0: Exit" -ForegroundColor Red
 $input = Read-Host "Please make a selection"
 Write-Host  -ForegroundColor Yellow "Loading OSDCloud..."
@@ -34,12 +32,10 @@ switch ($input)
 {
     '1' { Start-OSDCloud -OSLanguage en-gb -OSName 'Windows 10 22H2 x64' -OSEdition Education -ZTI } 
     '2' { Start-OSDCloud -OSLanguage en-gb -OSName 'Windows 10 22H2 x64' -OSEdition Enterprise -ZTI } 
-    '3' { Start-OSDCloud -OSLanguage en-gb -OSName 'Windows 11 22H2 x64' -OSEdition Education -ZTI }
-    '4' { Start-OSDCloud -OSLanguage en-gb -OSName 'Windows 11 22H2 x64' -OSEdition Enterprise -ZTI } 
-    '5' { Start-OSDCloud -OSLanguage en-gb -OSName 'Windows 11 23H2 x64' -OSEdition Education -ZTI }
-    '6' { Start-OSDCloud -OSLanguage en-gb -OSName 'Windows 11 23H2 x64' -OSEdition Enterprise -ZTI } 
-    '7' { Start-OSDCloud -OSLanguage en-gb -OSName 'Windows 11 23H2 x64' -OSEdition Professional -ZTI } 
-    '8' { Start-OSDCloudGUI	-Brand 'NOVUS'} 
+    '3' { Start-OSDCloud -OSLanguage en-gb -OSName 'Windows 11 23H2 x64' -OSEdition Education -ZTI }
+    '4' { Start-OSDCloud -OSLanguage en-gb -OSName 'Windows 11 23H2 x64' -OSEdition Enterprise -ZTI } 
+    '5' { Start-OSDCloud -OSLanguage en-gb -OSName 'Windows 11 23H2 x64' -OSEdition Professional -ZTI } 
+    '6' { Start-OSDCloudGUI	-Brand 'NOVUS'} 
     '0' { Exit }
 }
 
